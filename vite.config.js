b861fc90f1451ab1edc950cefbd836c0219dc16f
anonymus-devop <jerixortixdev@gmail.com>
2025-11-07 12:33:17 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/HorizonMaps/", // 👈 must match your repo name exactly!
+  base: "./", // ensures GitHub Pages paths resolve correctly
 });
