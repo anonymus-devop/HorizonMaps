167b03583f54b996116ddc40e239cd43ea6d040b
anonymus-devop <jerixortixdev@gmail.com>
2025-11-10 11:24:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HorizonMaps/', // VERY IMPORTANT for GitHub Pages
+  base: '/HorizonMaps/', // ✅ ensures correct asset paths on GitHub Pages
 })
