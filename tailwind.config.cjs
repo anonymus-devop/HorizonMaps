06ed4583d8fbf3e8109e00267fbede6f8184d527
anonymus-devop <jerixortixdev@gmail.com>
2025-11-10 10:48:02 -0500

    Refactor tailwind.config.cjs to CommonJS syntax
    Updated Tailwind CSS configuration to use CommonJS syntax and removed custom color and backdrop blur settings.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        horizonBlue: "#4facfe",
-        horizonViolet: "#8e2de2",
-      },
-      backdropBlur: {
-        xs: "2px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
