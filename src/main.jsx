0409d9a580600da4a98dfca2b77bdc4e258351f6
anonymus-devop <jerixortixdev@gmail.com>
2025-11-21 10:14:55 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import './styles.css';
+import App from "./App";
+import "./index.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
