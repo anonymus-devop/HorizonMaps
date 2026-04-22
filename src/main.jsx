96c2a134585851fd2ade2feaa300b21c7254d4fe
anonymus-devop <jerixortixdev@gmail.com>
2026-04-22 17:52:07 -0500

    Refactor imports and update ReactDOM rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import "mapbox-gl/dist/mapbox-gl.css";
+import "./styles/index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
